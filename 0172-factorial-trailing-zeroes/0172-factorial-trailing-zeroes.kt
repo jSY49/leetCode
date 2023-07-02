1a6bc810e7b907b6b70191495c01433bed135534
Yeon <96411866+jSY49@users.noreply.github.com>
2023-07-02 16:18:20 +0900

    Time: 114 ms (100.00%), Space: 33.2 MB (53.57%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.kt
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.kt
@@ -1,6 +1,8 @@
 class Solution {
     fun trailingZeroes(n: Int): Int {
-        
+        //trailing zero : 소수점 오른쪽에 있으면서 그 뒤에 숫자가 오지않는 0
+        //예 : 12200 에서 마지막00 , 13003000에서 000만 
+        //규칙 : n!에서 n을 5로 나누었을때 몫의 합 = tracing zero의 개수 
         var cnt =0 
         var res = n
         
