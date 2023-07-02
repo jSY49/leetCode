702870093cf2143fa327083065730109401ccc8d
Yeon <96411866+jSY49@users.noreply.github.com>
2023-07-02 16:40:47 +0900

    Time: 144 ms (11.71%), Space: 33.3 MB (54.72%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun climbStairs(n: Int): Int {
+        var dp = Array(n+1){0}
+        
+        if(n<3) return n
+        dp[0]=0
+        dp[1]=1
+        dp[2]=2
+        
+        for(i in 3 .. n){
+            dp[i]=(dp[i-1]+dp[i-2])
+            println("${dp[i]} = ${dp[i-1]} + ${dp[i-2]}")
+        }
+    
+        return dp[n]
+    }
+}
