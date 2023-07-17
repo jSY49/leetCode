f3dac7ec8946e0dd4c20d0e67819b78f1811fe0b
Yeon <96411866+jSY49@users.noreply.github.com>
2023-07-17 14:19:31 +0900

    Time: 1 ms (100.00%), Space: 45.2 MB (65.07%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int start=0;
+        int cnt=0;
+        
+        for(int i=0 ;i<nums.length;i++){
+            if(nums[i]!=0){
+                int tmp = nums[start];
+                nums[start] = nums[i];
+                nums[i] = tmp;
+                start++;
+             
+            }
+        }
+        
+    }
+}
