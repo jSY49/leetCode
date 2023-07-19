1ee894322fcc639381c8975eac044c799d7bf3a1
Yeon <96411866+jSY49@users.noreply.github.com>
2023-07-19 15:04:19 +0900

    Time: 0 ms (100.00%), Space: 43.7 MB (86.97%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -3,17 +3,11 @@ class Solution {
         int len = nums.length;
         int[] ans = new int[len];
         
-        int[] x = Arrays.copyOfRange(nums, 0, n);
-        int[] y = Arrays.copyOfRange(nums, n, len);
-        
-        System.out.println(x.length);
-        System.out.println(y.length);
-        
-        
         int j=0;
         for(int i =0 ; i<len;i+=2){
-            ans[i] = x[j];
-            ans[i+1] = y[j++];
+            ans[i] = nums[j];
+            ans[i+1] = nums[n+j];
+            j++;
         }
         
         return ans;
